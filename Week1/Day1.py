b76bf34d588ee88bcfef6f997b2681bd1084060a
GorillaPlayz <vikranth.bobba@gmail.com>
2024-08-01 21:30:20 -0400

    really big note btw

--- Week1/Day1.py
+++ Week1/Day1.py
@@ -1,2 +1,3 @@
-# Red line is bad check over it
+#The Input will get your input an use the print fuction to say Hello and see that you using single quotes with print then plus input("what is your name")
 print('Hello ' + input("What is your name")+ "!")
+
